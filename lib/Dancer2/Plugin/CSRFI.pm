10274a30d968699c0caaeeda92f29b1f0d0a1d13
Alexander Panteleev <alexpan@cpan.org>
2022-06-07 21:25:37 +0300

    [i] hook

--- lib/Dancer2/Plugin/CSRFI.pm
+++ lib/Dancer2/Plugin/CSRFI.pm
@@ -6,6 +6,7 @@ use warnings;
 
 use Dancer2::Plugin;
 use Dancer2::Core::Hook;
+use List::Util qw(any);
 use Crypt::SaltedHash;
 use Data::UUID;
 use namespace::clean;
@@ -13,6 +14,8 @@ use namespace::clean;
 our $VERSION = '0.01';
 
 plugin_keywords qw(csrf_token validate_csrf);
+
+plugin_hooks qw(after_validate_csrf);
 
 has session_key => (
     is      => 'ro',
@@ -38,21 +41,35 @@ has field_name => (
     default => sub { $_[0]->config->{field_name} || 'csrf_token' }
 );
 
-has response_status => (
+has send_error => (
+    is      => 'ro',
+    lazy    => 1,
+    default => sub { $_[0]->config->{show_error} || 0 }
+);
+
+has error_status => (
     is      => 'ro',
     lazy    => 1,
     default => sub { $_[0]->config->{response_status} || 403 }
 );
 
+has error_message => (
+    is      => 'ro',
+    lazy    => 1,
+    default => sub { $_[0]->config->{error_message} || 'Forbidden' }
+);
+
 sub BUILD {
     my ($self) = @_;
 
-    $self->app->add_hook(
-        Dancer2::Core::Hook->new(
-            name => 'before',
-            code => sub { $self->hook_before_request_validate_csrf(@_) }
-        )
-    );
+    if ($self->validate_post) {
+        $self->app->add_hook(
+            Dancer2::Core::Hook->new(
+                name => 'before',
+                code => sub {$self->hook_before_request_validate_csrf(@_)}
+            )
+        );
+    }
 }
 
 sub csrf_token {
@@ -85,7 +102,7 @@ sub validate_csrf {
     }
 
     my $unique   = $session->{unique};
-    my $entropy  = $self->page_entropy;
+    my $entropy  = $self->referer_entropy;
     my $expected = $self->generate($unique, $entropy);
 
     return $token eq $expected;
@@ -124,35 +141,78 @@ sub hash {
     return Crypt::SaltedHash->new;
 }
 
-
 sub hook_before_request_validate_csrf {
     my ($self, $app) = @_;
 
-    my $success = 0;
+    if (not $app->request->is_post) {
+        return;
+    }
+
+    my $content_type      = $app->request->content_type;
+    my @html_form_enctype = qw(application/x-www-form-urlencoded multipart/form-data);
+
+    if (not any { $_ eq $content_type } @html_form_enctype) {
+        return;
+    }
+
+    my $token   = $app->request->body_parameters->{$self->field_name};
+    my $success = $self->validate_csrf($token);
+    my $referer = $app->request->referer;
 
     if (not $success) {
         $self->app->log(
-            warning => __PACKAGE__ . ': Token is not valid'
+            info => {
+                message => __PACKAGE__ . ': Token is not valid',
+                referer => $referer,
+            }
         );
     }
     else {
         $self->app->log(
-            info => __PACKAGE__ . ': Token is valid'
+            debug => {
+                message => __PACKAGE__ . ': Token is valid',
+                referer => $referer,
+            }
         );
     }
 
     my %after_validate_bag = (
-        success => $success,
+        success       => $success,
+        referer       => $referer,
+        send_error    => $self->send_error,
+        error_status  => $self->error_status,
+        error_message => $self->error_message,
     );
 
     $self->app->log(
-        debug => __PACKAGE__ . ': Entering after_validate_csrf hook'
+        debug => {
+            message => __PACKAGE__ . ': Entering after_validate_csrf hook',
+            referer => $referer,
+        }
     );
 
     $self->execute_plugin_hook(
         'after_validate_csrf',
         $app,
         \%after_validate_bag,
+    );
+
+    if (not $after_validate_bag{show_error}) {
+        return;
+    }
+
+    $self->app->log(
+        info => {
+            message       => __PACKAGE__ . ': Sending error',
+            referer       => $referer,
+            error_status  => $after_validate_bag{error_status},
+            error_message => $after_validate_bag{error_message},
+        }
+    );
+
+    $app->send_error(
+        $after_validate_bag{error_message},
+        $after_validate_bag{error_status},
     );
 }
 
